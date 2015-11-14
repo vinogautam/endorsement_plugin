d95fdede3c535803211da001aef3e7b8f7ec3ac3
vinogautam <dhanavel237vino@gmail.com>
2015-11-14 21:45:06 +0530

    Settings and Edit delete options for endorsers/letter templates

--- admin/ntm-admin.php
+++ admin/ntm-admin.php
@@ -13,7 +13,7 @@ class Endorsements_admin{
             add_menu_page( 'Endorsements', 'Endorsements', 'manage_options', 'ntmEndorsements', array( $this, 'create_ntmadmin_page' ));
 			add_submenu_page( 'ntmEndorsements', 'Endorsements', 'Add Endorser',  9, 'ntmEndorsements&tab=add_endorsers', array( &$this, 'mail_template'));
 			add_submenu_page( 'ntmEndorsements', 'Endorsements', 'Email Template',  9, 'mail_template', array( &$this, 'mail_template'));
-			add_submenu_page( 'ntmEndorsements', 'Endorsements', 'Settings',  9, 'settings', array( &$this, 'settingsPage'));		
+			add_submenu_page( 'ntmEndorsements', 'Endorsements', 'Settings',  9, 'ntmEndorsements_settings', array( &$this, 'settingsPage'));		
         
         } else {
             
@@ -24,41 +24,23 @@ class Endorsements_admin{
     } //end ntme_admin_menu()
     
     //our admin tabs navigation
-    public function adminTabs($tabs){
+    public function adminTabs($tabs, $default, $page){
         
-        if ( isset ( $_GET['tab'] ) ) $current = $_GET['tab']; else $current = 'general';
+        if ( isset ( $_GET['tab'] ) ) $current = $_GET['tab']; else $current = $default;
         
         echo '<div id="icon-themes" class="icon32"><br></div>';
         echo '<h2 class="nav-tab-wrapper">';
     
         foreach( $tabs as $tab => $name ){
             $class = ( $tab == $current ) ? ' nav-tab-active' : '';
-            echo "<a class='nav-tab$class' href='?page=ntmEndorsements&tab=$tab'>$name</a>";
+            echo "<a class='nav-tab$class' href='?page=$page&tab=$tab'>$name</a>";
     
         }
         
         echo '</h2>';
     }
-    
-    //our AGENTS tabs navigation
-    public function advisorTabs($current){
-        
-        if ( isset ( $_GET['tab'] ) ) $current = $_GET['tab']; else $current = 'general';
-        
-         $tabs = array( 'general' => 'Gift Manager', 'endorsers' => 'Endorsers', 'conversions' => 'New Conversions' );
-    
-        echo '<div id="icon-themes" class="icon32"><br></div>';
-        echo '<h2 class="nav-tab-wrapper">';
-    
-        foreach( $tabs as $tab => $name ){
-            $class = ( $tab == $current ) ? ' nav-tab-active' : '';
-            echo "<a class='nav-tab$class' href='?page=ntmEndorsements&tab=$tab'>$name</a>";
-    
-        }
-        
-        echo '</h2>';
-    }
-    
+	
+       
     /**
      * admin page callback
      */
@@ -77,7 +59,7 @@ class Endorsements_admin{
             <h2><?php echo $current_page;?></h2>           
             <?php 
 				if(isset($error)) echo $error;
-				$this->adminTabs($tabs);
+				$this->adminTabs($tabs, 'endorsers', 'ntmEndorsements');
 				$this->$current_tab();
 			?>
         </div>
@@ -87,11 +69,14 @@ class Endorsements_admin{
 	
 	public function endorsers_page()
     {
-		$endosersTable = new EndoserTable();
-		$endosersTable->prepare_items();
-		$endosersTable->display();
 		?>
-		
+		<form method="post">
+		<?php
+			$endosersTable = new EndoserTable();
+			$endosersTable->prepare_items();
+			$endosersTable->display();
+		?>
+		</form>
 		<?php
 	}
     
@@ -100,7 +85,13 @@ class Endorsements_admin{
 		global $wpdb;
 		
 		$endorser_template = $wpdb->get_results("select * from ".$wpdb->prefix . "mailtemplates where type='Endorser'");
-		$endorsement_template = $wpdb->get_results("select * from ".$wpdb->prefix . "mailtemplates where type!='Endorser'")
+		$endorsement_template = $wpdb->get_results("select * from ".$wpdb->prefix . "mailtemplates where type!='Endorser'");
+		
+		if(isset($_GET['edit']))
+		{	
+			$user = get_userdata($_GET['edit']);
+			$usermeta = get_user_meta($_GET['edit']);
+		}
 		
 		?>
 		<form method="post" action="<?php admin_url( 'admin.php?page=ntmEndorsements' ); ?>">
@@ -108,30 +99,30 @@ class Endorsements_admin{
 				<tbody>
 					<tr>
 						<th scope="row"><label for="blogname">Firstname</label></th>
-						<td><input type="text" class="regular-text" value="" id="blogname" name="user[first_name]"></td>
+						<td><input type="text" class="regular-text" value="<?php echo isset($usermeta) ? $usermeta['first_name'][0]: '';?>" id="blogname" name="user[first_name]"></td>
 					</tr>
 					<tr>
 						<th scope="row"><label for="blogname">Lastname</label></th>
-						<td><input type="text" class="regular-text" value="" id="blogname" name="user[last_name]"></td>
+						<td><input type="text" class="regular-text" value="<?php echo isset($usermeta) ? $usermeta['last_name'][0]: '';?>" id="blogname" name="user[last_name]"></td>
 					</tr>
 					<tr>
 						<th scope="row"><label for="blogname">Email</label></th>
-						<td><input type="text" class="regular-text" value="" id="blogname" name="user[user_email]"></td>
+						<td><input type="text" class="regular-text" <?php echo isset($user) ? 'disabled' : '';?> value="<?php echo isset($user) ? $user->user_email: '';?>" id="blogname" name="user[user_email]"></td>
 					</tr>
 					<tr>
 						<th scope="row"><label for="blogname">Endorser Letter</label></th>
-						<td><select class="regular-text" name="letter[type]">
+						<td><select class="regular-text" name="endorser_letter">
 								<?php foreach($endorser_template as $r){?>
-								<option value="<?php _e($r->id);?>"><?php _e($r->name);?></option>
+								<option <?php echo isset($usermeta) && $usermeta['endorser_letter'][0] == $r->id ? 'selected' : '';?> value="<?php _e($r->id);?>"><?php _e($r->name);?></option>
 								<?php }?>
 							</select>
 						</td>
 					</tr>
 					<tr>
 						<th scope="row"><label for="blogname">Endorsement Letter</label></th>
-						<td><select class="regular-text" name="letter[type]">
+						<td><select class="regular-text" name="endorsement_letter">
 								<?php foreach($endorsement_template as $r){?>
-								<option value="<?php _e($r->id);?>"><?php _e($r->name);?></option>
+								<option <?php echo isset($usermeta) && $usermeta['endorsement_letter'][0] == $r->id ? 'selected' : '';?> value="<?php _e($r->id);?>"><?php _e($r->name);?></option>
 								<?php }?>
 							</select>
 						</td>
@@ -145,16 +136,23 @@ class Endorsements_admin{
 	
 	public function template_list_page()
     {
-		$endosersTable = new LetterTable();
-		$endosersTable->prepare_items();
-		$endosersTable->display();
 		?>
-		
+		<form method="post">
+		<?php
+			$endosersTable = new LetterTable();
+			$endosersTable->prepare_items();
+			$endosersTable->display();
+		?>
+		</form>
 		<?php
 	}
     
     public function add_template_page()
     {
+		global $wpdb;
+		
+		if(isset($_GET['edit']))
+			$template = $wpdb->get_row("select * from ".$wpdb->prefix . "mailtemplates where id='".$_GET['edit']."'");
 		?>
 		<link rel="stylesheet" type="text/css" href="<?php _e(NTM_PLUGIN_URL);?>/assets/css/ckeditor.css" media="all" />
 		<script type='text/javascript' src='<?php _e(NTM_PLUGIN_URL);?>/assets/js/ckeditor/ckeditor.js'></script>
@@ -164,23 +162,23 @@ class Endorsements_admin{
 					<tr>
 						<th scope="row"><label for="blogname">Type</label></th>
 						<td><select class="regular-text" name="letter[type]">
-								<option>Endorser</option>
-								<option>Endorsement</option>
+								<option <?php echo isset($template) && $template->type == 'Endorser' ? 'selected' : '';?>>Endorser</option>
+								<option <?php echo isset($template) && $template->type == 'Endorsement' ? 'selected' : '';?>>Endorsement</option>
 							</select>
 						</td>
 					</tr>
 					<tr>
 						<th scope="row"><label for="blogname">Letter Name</label></th>
-						<td><input type="text" class="regular-text" value="" id="blogname" name="letter[name]"></td>
+						<td><input type="text" class="regular-text" value="<?php echo isset($template) ? $template->name: '';?>" id="blogname" name="letter[name]"></td>
 					</tr>
 					<tr>
 						<th scope="row"><label for="blogname">Subject</label></th>
-						<td><input type="text" class="regular-text" value="" id="blogname" name="letter[subject]"></td>
+						<td><input type="text" class="regular-text" value="<?php echo isset($template) ? $template->subject: '';?>" id="blogname" name="letter[subject]"></td>
 					</tr>
 					<tr>
 						<th scope="row"><label for="blogname">Content</label></th>
 						<td>
-							<textarea cols="80" id="editor" name="" rows="10" name="letter[content]"><?php _e($mailtemplate['content']);?></textarea>
+							<textarea cols="80" id="editor" rows="10" name="letter[content]"><?php echo isset($template) ? $template->content: '';?></textarea>
 							<script>
 								CKEDITOR.replace( 'editor' );
 							</script>
@@ -280,7 +278,14 @@ class Endorsements_admin{
 	{
 		global $wpdb;
 		
-		if(isset($_POST['submit']))
+		if(isset($_POST['submit']) && isset($_GET['edit']))
+		{
+			update_user_meta($_GET['edit'], 'endorser_letter', $_POST['endorser_letter']);
+			update_user_meta($_GET['edit'], 'endorsement_letter', $_POST['endorsement_letter']);
+			update_user_meta($_GET['edit'], 'first_name', $_POST['user']['first_name']);
+			update_user_meta($_GET['edit'], 'last_name', $_POST['user']['last_name']);
+		}
+		elseif(isset($_POST['submit']))
 		{
 			$user = $_POST['user'];
 			$user['role'] = 'endorser';
@@ -295,6 +300,8 @@ class Endorsements_admin{
 				}
 				else
 				{
+					update_user_meta($user_id, 'endorser_letter', $_POST['endorser_letter']);
+					update_user_meta($user_id, 'endorsement_letter', $_POST['endorsement_letter']);
 					$ntm_mail->send_welcome_mail($user['user_email'], $user_id, $user['user_login'].'#'.$user['user_pass']);
 					$ntm_mail->send_notification_mail($user_id);
 				}
@@ -312,8 +319,94 @@ class Endorsements_admin{
 		}
 		elseif(isset($_POST['letter-save']))
 		{
-			$_POST['letter']['created'] = date("Y-m-d H:i:s");
-			$wpdb->insert($wpdb->prefix . "mailtemplates", $_POST['letter']);
+			if(isset($_GET['edit']))
+			{
+				$wpdb->update($wpdb->prefix . "mailtemplates", $_POST['letter'], array('id' => $_GET['edit']));
+			}
+			else
+			{
+				$_POST['letter']['created'] = date("Y-m-d H:i:s");
+				$wpdb->insert($wpdb->prefix . "mailtemplates", $_POST['letter']);
+			}
+		}
+		elseif(isset($_GET['tab']) && $_GET['tab'] == 'template_list' && isset($_GET['delete']))
+			$wpdb->delete($wpdb->prefix . "mailtemplates", array( 'id' => $_GET['delete'] ) );
+		elseif(isset($_GET['tab']) && $_GET['tab'] == 'endorsers' && isset($_GET['delete']))
+			wp_delete_user($_GET['delete']);
+	}
+	
+	public function settingsPage()
+    {   global $pagenow, $current_user, $ntm_mail;
+		if ( isset ( $_GET['tab'] ) ) $current = $_GET['tab']; else $current = 'cloudsponge';
+		
+		$tabs = array( 'cloudsponge' => 'Cloudsponge', 'giftbit' => 'Giftbit');
+		$current_page = $tabs[$current];
+		$current_tab = $current.'_settings';
+		
+		$error = $this->settings_actions();
+		
+		?>
+        <div class="wrap">
+            <h2><?php echo $current_page;?></h2>           
+            <?php 
+				if(isset($error)) echo $error;
+				$this->adminTabs($tabs, 'cloudsponge', 'ntmEndorsements_settings');
+				$this->$current_tab();
+			?>
+        </div>
+        <?php
+        
+    }
+	
+	public function cloudsponge_settings()
+	{
+		$option = get_option('cloudsponge');
+		?>
+		<form method="post">
+			<table class="form-table">
+				<tbody>
+					<tr>
+						<th scope="row"><label for="blogname">Javscript Access key</label></th>
+						<td><input type="text" class="regular-text" value="<?php echo $option;?>" id="blogname" name="cloudsponge"></td>
+					</tr>
+				</tbody>
+			</table>
+			<?php submit_button('Save Letter', 'primary', 'cloudsponge-save');?>
+		</form>
+		<?php
+	}
+	
+	public function giftbit_settings()
+	{
+		$option = get_option('giftbit');
+		?>
+		<form method="post">
+			<table class="form-table">
+				<tbody>
+					<tr>
+						<th scope="row"><label for="blogname">Api key</label></th>
+						<td><input type="text" class="regular-text" value="<?php echo isset($option['api']) ? $option['api'] : '';?>" id="blogname" name="giftbit[api]"></td>
+					</tr>
+					<tr>
+						<th scope="row"><label for="blogname">Sandbox</label></th>
+						<td><input type="checkbox" class="regular-text" <?php echo isset($option['sandbox']) ? 'checked' : '';?> value="1" id="blogname" name="giftbit[sandbox]"></td>
+					</tr>
+				</tbody>
+			</table>
+			<?php submit_button('Save Letter', 'primary', 'giftbit-save');?>
+		</form>
+		<?php
+	}
+	
+	public function settings_actions()
+	{
+		if(isset($_POST['cloudsponge-save']))
+		{
+			update_option('cloudsponge', $_POST['cloudsponge']);
+		}
+		elseif(isset($_POST['giftbit-save']))
+		{
+			update_option('giftbit', $_POST['giftbit']);
 		}
 	}
         

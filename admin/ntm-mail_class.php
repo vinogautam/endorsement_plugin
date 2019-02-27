a5167b635b1ce6b4b1197bd612fca34b8e24eed2
vinogautam <dhanavel237vino@gmail.com>
2019-02-27 21:25:49 +0530

    Common mail template

--- admin/ntm-mail_class.php
+++ admin/ntm-mail_class.php
@@ -347,26 +347,153 @@ Let me know if you have any questions,", '');
 	}
 	
 	public function get_mail_template($content, $preheader_text=''){
-		
-		$value = '<html>
-		<head>
-			<style>'.stripslashes(strip_tags(get_option('mail_template_css'))).'</style>
-		</head>
-		<body>
-			<!-- Visually Hidden Preheader Text : BEGIN -->
-	        <div style="display: none; font-size: 1px; line-height: 1px; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; mso-hide: all; font-family: sans-serif;">
-	            '.$preheader_text.'
-	        </div>
-	        <!-- Visually Hidden Preheader Text : END -->
-			<div>'.$content.'</div>
-		</body>
-		</html>';
 
-		/*$value = '<table cellpadding="5" width="750" cellspacing="0" border="0" style="background-color:#FFF;">
-				<td align="left" colspan="4" style="margin-right:30px; padding-left:10px;">'.$content.'</td>
-				</tr>
-                <tr><td colspan="4" align="center" style="background-color:#EAEAEA; border-top: solid 3px #1E1F22; padding-top:10px; color:#000;">Poweredby Financial Insiders</td></tr>
-</table>';*/
+		$value = "<!doctype html>
+<html>
+  <head>
+    <meta name='viewport' content='width=device-width'>
+    <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <title>Simple Transactional Email</title>
+    <style>
+    /* -------------------------------------
+        INLINED WITH htmlemail.io/inline
+    ------------------------------------- */
+    /* -------------------------------------
+        RESPONSIVE AND MOBILE FRIENDLY STYLES
+    ------------------------------------- */
+    @media only screen and (max-width: 620px) {
+      table[class=body] h1 {
+        font-size: 28px !important;
+        margin-bottom: 10px !important;
+      }
+      table[class=body] p,
+            table[class=body] ul,
+            table[class=body] ol,
+            table[class=body] td,
+            table[class=body] span,
+            table[class=body] a {
+        font-size: 16px !important;
+      }
+      table[class=body] .wrapper,
+            table[class=body] .article {
+        padding: 10px !important;
+      }
+      table[class=body] .content {
+        padding: 0 !important;
+      }
+      table[class=body] .container {
+        padding: 0 !important;
+        width: 100% !important;
+      }
+      table[class=body] .main {
+        border-left-width: 0 !important;
+        border-radius: 0 !important;
+        border-right-width: 0 !important;
+      }
+      table[class=body] .btn table {
+        width: 100% !important;
+      }
+      table[class=body] .btn a {
+        width: 100% !important;
+      }
+      table[class=body] .img-responsive {
+        height: auto !important;
+        max-width: 100% !important;
+        width: auto !important;
+      }
+    }
+
+    /* -------------------------------------
+        PRESERVE THESE STYLES IN THE HEAD
+    ------------------------------------- */
+    @media all {
+      .ExternalClass {
+        width: 100%;
+      }
+      .ExternalClass,
+            .ExternalClass p,
+            .ExternalClass span,
+            .ExternalClass font,
+            .ExternalClass td,
+            .ExternalClass div {
+        line-height: 100%;
+      }
+      .apple-link a {
+        color: inherit !important;
+        font-family: inherit !important;
+        font-size: inherit !important;
+        font-weight: inherit !important;
+        line-height: inherit !important;
+        text-decoration: none !important;
+      }
+      .btn-primary table td:hover {
+        background-color: #34495e !important;
+      }
+      .btn-primary a:hover {
+        background-color: #34495e !important;
+        border-color: #34495e !important;
+      }
+    }
+    </style>
+  </head>
+  <body class='' style='background-color: #f6f6f6; font-family: sans-serif; -webkit-font-smoothing: antialiased; font-size: 14px; line-height: 1.4; margin: 0; padding: 0; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%;'>
+    <table border='0' cellpadding='0' cellspacing='0' class='body' style='border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background-color: #f6f6f6;'>
+      <tr>
+        <td style='font-family: sans-serif; font-size: 14px; vertical-align: top;'>&nbsp;</td>
+        <td class='container' style='font-family: sans-serif; font-size: 14px; vertical-align: top; display: block; Margin: 0 auto; max-width: 580px; padding: 10px; width: 580px;'>
+          <div class='content' style='box-sizing: border-box; display: block; Margin: 0 auto; max-width: 580px; padding: 10px;'>
+
+            <!-- START CENTERED WHITE CONTAINER -->
+            <span class='preheader' style='color: transparent; display: none; height: 0; max-height: 0; max-width: 0; opacity: 0; overflow: hidden; mso-hide: all; visibility: hidden; width: 0;'>".$preheader_text."</span>
+            <table class='main' style='border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background: #ffffff; border-radius: 3px;'>
+
+              <!-- START MAIN CONTENT AREA -->
+              <tr>
+                <td class='wrapper' style='font-family: sans-serif; font-size: 14px; vertical-align: top; box-sizing: border-box; padding: 20px;'>
+                  <table border='0' cellpadding='0' cellspacing='0' style='border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;'>
+                    <tr>
+                      <td style='font-family: sans-serif; font-size: 14px; vertical-align: top;'>
+                        <p style='font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;'>Hi [USERNAME],</p>
+                        <p style='font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;'>
+                          ".$content."
+                        </p>
+                        <p style='font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;'>[PERSONALNOTE].</p>
+                        <p style='font-family: sans-serif; font-size: 14px; font-weight: normal; margin: 0; Margin-bottom: 15px;'>Good luck! Hope it works.</p>
+                      </td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+
+            <!-- END MAIN CONTENT AREA -->
+            </table>
+
+            <!-- START FOOTER -->
+            <div class='footer' style='clear: both; Margin-top: 10px; text-align: center; width: 100%;'>
+              <table border='0' cellpadding='0' cellspacing='0' style='border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;'>
+                <tr>
+                  <td class='content-block' style='font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;'>
+                    <span class='apple-link' style='color: #999999; font-size: 12px; text-align: center;'>Company Inc, 3 Abbey Road, San Francisco CA 94102</span>
+                    <br> Don't like these emails? <a href='http://i.imgur.com/CScmqnj.gif' style='text-decoration: underline; color: #999999; font-size: 12px; text-align: center;'>Unsubscribe</a>.
+                  </td>
+                </tr>
+                <tr>
+                  <td class='content-block powered-by' style='font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;'>
+                    Powered by <a href='http://htmlemail.io' style='color: #999999; font-size: 12px; text-align: center; text-decoration: none;'>HTMLemail</a>.
+                  </td>
+                </tr>
+              </table>
+            </div>
+            <!-- END FOOTER -->
+
+          <!-- END CENTERED WHITE CONTAINER -->
+          </div>
+        </td>
+        <td style='font-family: sans-serif; font-size: 14px; vertical-align: top;'>&nbsp;</td>
+      </tr>
+    </table>
+  </body>
+</html>";
 
 		return $value;
 	}
@@ -376,6 +503,8 @@ Let me know if you have any questions,", '');
 					
 		global $wpdb;
 		
+		$user = count($_POST) ? $_POST : (array) json_decode(file_get_contents('php://input'));
+
 		$user_info = get_userdata($user_id);
       	
 		$username = $user_info->user_login;
@@ -385,37 +514,22 @@ Let me know if you have any questions,", '');
 		$blog_id = get_active_blog_for_user( $user_id )->blog_id;
 		$agent_id = get_blog_option($blog_id, 'agent_id');
 		$agent_info = get_userdata($agent_id);
-		
-		//print_r(get_permalink(get_option('ENDORSEMENT_FRONT_END')).'?autologin='.base64_encode(base64_encode($autologin)));
-		/*$endorser_letter = get_user_meta($user_id, 'endorser_letter', true);
-		if($endorser_letter)
-		{
-			$res = $wpdb->get_row("select * from ".$wpdb->prefix . "mailtemplates where id=".$endorser_letter);
-			$subject = isset($res->subject) ? $res->subject : $data['subject'];
-			$content = isset($res->content) ? $res->content : $data['content'];
-		}
-		else
-		{
-			$subject = $data['subject'];
-			$content = $data['content'];
-		}*/
 
-		$campaign = get_user_meta($user_id, 'campaign', true);
-		//$templates = $wpdb->get_row("select * from ".$wpdb->prefix."campaign_templates where name = 'Endorser Invitation' and campaign_id=".$campaign);
+		$botId = get_user_meta($user_id, 'campaign', true);
 
-		$botInfo = get_post_meta($campaign, 'emailInvite', true);
+		$botInfo = get_post_meta($botId, 'emailInvite', true);
 		$botInfo = $botInfo ? (array) $botInfo : array();
 		$video = '';
 		if($videoURL) {
 			$video = $videoURL;	
 		}
 
-		$subject = stripslashes(stripslashes($botInfo->subject)) ? stripslashes(stripslashes($botInfo->subject)) : 'Welcome to financialinsiders';
-		$preheader_text = stripslashes(stripslashes($botInfo->preheader));
-		$content = str_replace("<br />", "", stripslashes(stripslashes($botInfo->body)));
+		$subject = stripslashes(stripslashes($botInfo['subject'])) ? stripslashes(stripslashes($botInfo['subject'])) : 'Welcome to financialinsiders';
+		$preheader_text = stripslashes(stripslashes($botInfo['preheader']));
+		$content = str_replace("<br />", "", stripslashes(stripslashes($botInfo['body'])));
 
 		$content 	=	str_ireplace('[ENDORSER]', get_user_meta($user_id, 'first_name', true).' '.get_user_meta($user_id, 'last_name', true), $content);
-		$content 	=	str_ireplace('[AUTO_LOGIN_LINK]', (get_option('endorser_app') ? get_option('endorser_app') : get_permalink(get_option('ENDORSEMENT_FRONT_END'))).'?autologin='.base64_encode(base64_encode($autologin)).'&video='.$video, $content);
+		$content 	=	str_ireplace('[AUTO_LOGIN_LINK]', get_permalink($botId).'?autologin='.base64_encode(base64_encode($autologin)).'&videoURL='.$video.'&messagetxt='.$user['landingPageContent'], $content);
 		$content 	=	str_ireplace('[AGENT]', $agent_info->user_login, $content);
 		$content 	=	str_ireplace('[AGENT_EMAIL]', $agent_info->user_email, $content);				
 		$content	= 	str_ireplace('[SITE]', get_option('blogname'), $content);
@@ -468,11 +582,39 @@ Let me know if you have any questions,", '');
 		
 	}
 	
+	public function send_endorser_invitation_mail($mail){
+					
+		global $current_user, $wpdb;
+
+		$_POST = (array) json_decode(file_get_contents('php://input'));
+
+		$botInfo = get_post_meta($_POST['botId'], 'emailInvite', true);
+		$botInfo = $botInfo ? (array) $botInfo : array();
+
+		$pagelink = get_permalink($_POST['botId']);
+
+		$subject = stripslashes(stripslashes($botInfo->subject)) ? stripslashes(stripslashes($botInfo->subject)) : 'Welcome to financialinsiders';
+		$preheader_text = stripslashes(stripslashes($botInfo->preheader));
+		$content = str_replace("<br />", "", stripslashes(stripslashes($botInfo->body)));
+		
+		$content 	=	str_ireplace('[INVITELINK]', $pagelink.'?ref='.base64_encode(base64_encode($mail)), $content);
+		$content	= 	str_ireplace('[SITE]', get_option('blogname'), $content);
+
+		$message	=	$this->get_mail_template($content, $preheader_text);
+		
+		if($this->send_mail($mail, $subject , $message, $fromName=Null, $fromEmail=Null))
+		return true;
+		else
+		return false;
+	}
+
 	public function send_invitation_mail($info, $endorser, $id, $content=''){
 					
 		global $current_user, $wpdb;
 		
 		$data = $this->get_invitation_mail();
+
+		$_POST = (array) json_decode(file_get_contents('php://input'));
 		
 		/*$endorser_letter = get_user_meta($endorser, 'endorsement_letter', true);
 		if($endorser_letter)
@@ -489,19 +631,19 @@ Let me know if you have any questions,", '');
 			$pagelink = get_option('ENDORSEMENT_FRONT_END');
 		}*/
 
-		
-			$campaign = get_user_meta($endorser, 'campaign', true);
-			$dcampaign = $wpdb->get_row("select * from ".$wpdb->prefix."campaigns where id=".$campaign);
-			$templates = $wpdb->get_row("select * from ".$wpdb->prefix."campaign_templates where name = 'Endorsement Letter' and campaign_id=".$campaign);
 
-			$pagelink = get_post_meta($dcampaign->strategy, 'strategy_link', true);
-
-			$subject = stripslashes(stripslashes($templates->subject)) ? stripslashes(stripslashes($templates->subject)) : 'Welcome to financialinsiders';
-			$preheader_text = stripslashes(stripslashes($templates->preheader_text));
-
-		if($content == ''){
-			$content = str_replace("<br />", "", stripslashes(stripslashes($templates->template)));
+		$botInfo = get_post_meta($_POST['botId'], 'emailInvite', true);
+		$botInfo = $botInfo ? (array) $botInfo : array();
+		$video = '';
+		if($videoURL) {
+			$video = $videoURL;	
 		}
+
+		$pagelink = get_permalink($_POST['botId']);
+
+		$subject = stripslashes(stripslashes($botInfo->subject)) ? stripslashes(stripslashes($botInfo->subject)) : 'Welcome to financialinsiders';
+		$preheader_text = stripslashes(stripslashes($botInfo->preheader));
+		$content = str_replace("<br />", "", stripslashes(stripslashes($botInfo->body)));
 
 		$video = $templates->media ? $templates->media : get_user_meta($endorser, 'video', true) ;
 		
